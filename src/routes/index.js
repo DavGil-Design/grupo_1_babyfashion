62538a9276860ac6509ce59d1da3bd957687e482
DaviGil <davids@gilsbg@gmail.com>
2021-02-14 22:19:49 -0500

    Master trend & suggest dinamic article works

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,68 @@ router.get('/', function(req, res, next) {
 
   let categories = ['ropa','Accesorios','Caminadores','Carreolas','Neceser'];
   let subTitle = ['trend','suggested'];
-  let subTitles_title = ['Tendencias','Sugeridos'];
+  let subTitle_title = ['Tendencias','Sugeridos']
+  let trendProduct = [
+    {
+      name: 'Caminador1',
+      description: 'Camiandor1-D',
+      price: '15.000',
+      stockProducts: '3'
+    },
+    {
+      name: 'Ropa1',
+      description: 'Ropa1-D',
+      price: '5.000',
+      stockProducts: '2'
+    },
+  ];
+  let suggestedProduct = [
+    {
+      name: 'Caminador1',
+      description: 'Camiandor1-D',
+      price: '15.000',
+      stockProducts: '3'
+    },
+    {
+      name: 'Ropa1',
+      description: 'Ropa1-D',
+      price: '5.000',
+      stockProducts: '2'
+    },
+  ];
+
+  // let subTitle_title = [
+  //   'Tendencias' = {
+  //     productSeacrh: function([i]){
+  //       let product = [];
+  //       .find()
+  //     }
+  //   },
+  //   'Sugeridos' = {}
+  // ];
+
+
+  // for (let i = 0; i < product.length; i++) {
+  //     let subTitle_title = [{
+  //     Tendencias: 'product[i]'
+  //   },
+  //   {
+  //     Sugerido:'product[i]'
+  //   }
+      
+  // }
+
+  
+  
+
 
   res.render('index', {
     title: 'BabyFashion',
     categories: categories,
     subTitle : subTitle,
-    subTitles_title: subTitles_title,
-
+    subTitle_title: subTitle_title,
+    trendProduct: trendProduct,
+    suggestedProduct: suggestedProduct
   });
 });
 
